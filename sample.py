9c89625c18703fd121440a25e605b487a67c5367
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2011-09-26 20:19:44 -0400

    using context manager

--- sample.py
+++ sample.py
@@ -3,14 +3,10 @@ import xnnppx
 # xnnppx.arguments
 # xnnppx.parameters
 
-try:
-    xnnppx.workflow_info.update('0', 'start', 0)
+xnnppx.workflow_info.update('0', 'start', 0)
+
+with xnnppx.ContextManager():
     # do stuff here...
-except:
-    if xnnppx.log_file is None:
-        xnnppx.workflow_info.fail()
-    else:
-        xnnppx.workflow_info.fail('see %s for errors' % xnnppx.log_file)
-    raise
+    pass
 
 # eof
